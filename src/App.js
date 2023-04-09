c752ca2cf2c78a2b527900b8474dd78858350de8
Jacob Squier <jacob.squier@gmail.com>
2023-04-09 00:08:19 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import './styles/App.css';
+import './styles/widget.css'
+// Component imports below
+import PhotoWidget from './components/PhotoWidget';
+import FollowerWidget from './components/FollowerWidget';
+import Media from './components/Media';
+import Head from './components/Head';
 
 function App() {
+
+  const apps = [
+    {
+      title: "Instagram",
+      id: "instagramApp",
+      url: "",
+    },
+    {
+      title: "Tiktok",
+      id: "tiktokApp",
+      url: "",
+    },
+    {
+      title: "Twitch",
+      id: "twitchApp",
+      url: "",
+    },
+    {
+      title: "Snapchat",
+      id: "snapchatApp",
+      url: "",
+    }
+  ]
+
   return (
     <div className="App">
-      <div id="main-container">
+      <div id="container">
+        <div id="head">
+          <Head />
+        </div>
+        <div id="main">
+          <PhotoWidget />
+          <FollowerWidget />
+          {
+            apps.map((app) => {
+              return (
+                <Media title={app.title} id={app.id} url={app.url}/>
+              )
+            })
+          }
+        </div>
+        <div id="foot">
 
+        </div>
       </div>
     </div>
   );
